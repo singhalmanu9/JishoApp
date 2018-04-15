3632e313c0b0bf8c0d85a4cc54aa93530ff33dd8
Abhimanyu Singhal <singhalmanu9@gmail.com>
2018-04-14 20:31:20 -0700

    added img and about

--- Jisho/app/src/main/java/c/example/jisho/SearchActivity.java
+++ Jisho/app/src/main/java/c/example/jisho/SearchActivity.java
@@ -35,4 +35,9 @@ public class SearchActivity extends AppCompatActivity {
         }
     }
 
+    public void openAbout(View view) {
+        Intent i = new Intent(this, AboutActivity.class);
+        startActivity(i);
+    }
+
 }
