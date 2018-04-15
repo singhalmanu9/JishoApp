06e84f564a95cce744e4e6d286a2dcc0df77edad
Abhimanyu Singhal <singhalmanu9@gmail.com>
2018-04-14 20:51:59 -0700

    added empty radsearch

---
+++ Jisho/app/src/main/java/c/example/jisho/RadSearchActivity.java
@@ -0,0 +1,13 @@
+package c.example.jisho;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RadSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rad_search);
+    }
+}
