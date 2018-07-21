a86509ca0c941a6f055803490afad65d8a5f8ab4
unknown <timothytoombs@berkeley.edu>
2018-07-21 10:20:01 -0700

    added beginning of search window

--- jishof/lib/main.dart
+++ jishof/lib/main.dart
@@ -7,25 +7,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.lightGreen,
-      ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Flutter Demo',
+        theme: new ThemeData(
+          primarySwatch: Colors.lightGreen,
+        ),
+        home: new MyHomePage(),
+        routes: <String, WidgetBuilder>{
+          '/defaultSearch': (BuildContext context) =>
+              new DefaultSearchPage(_MyHomePageState.searchBarController.text)
+        });
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({Key key}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -36,24 +31,17 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-  final String title;
-
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  static TextEditingController searchBarController =
+      new TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _search() {
+    Navigator.pushNamed(context, '/defaultSearch');
   }
 
   @override
@@ -65,11 +53,6 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
       body: new Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
@@ -89,24 +72,18 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            new Padding (
-                padding: new EdgeInsets.symmetric(
-                    vertical: 0.0,
-                    horizontal: 40.0
-                ),
-                child:new TextField(
+            new Padding(
+                padding:
+                    new EdgeInsets.symmetric(vertical: 0.0, horizontal: 40.0),
+                child: new SearchTextField(
+                    child: new TextField(
                   decoration: const InputDecoration(
-                      border:  const OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Color(0x00000000)
-                          )
-                      ),
-                      labelText: "Type your query here:"
-                  ),
+                      border: const OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0x00000000))),
+                      labelText: "Type your query here:"),
                   textAlign: TextAlign.left,
-
-                )
-            ),
+                  controller: searchBarController,
+                ))),
             new Text(
               '$_counter',
               style: Theme.of(context).textTheme.display1,
@@ -115,10 +92,33 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: _search,
+        tooltip: 'Search',
+        child: new Icon(Icons.search),
+      ),
     );
+  }
+}
+
+class SearchTextField extends InheritedWidget {
+  const SearchTextField({Key key, @required Widget child})
+      : assert(child != null),
+        super(key: key, child: child);
+
+  final TextField textField;
+
+  @override
+  bool updateShouldNotify(SearchTextField stv) => textField != stv.textField;
+}
+
+class DefaultSearchPage extends StatelessWidget {
+  String searchTextField;
+
+  DefaultSearchPage(String searchTextField)
+      : this.searchTextField = searchTextField;
+
+  Widget build(BuildContext context) {
+    return new Text(
+        searchTextField); //TODO change this into the search process. dynamically build the list of widgets from thereon.
   }
 }
