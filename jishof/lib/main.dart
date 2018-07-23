c117886b7d8910d17be4b201375de63e31896a5c
unknown <timothytoombs@berkeley.edu>
2018-07-23 16:32:56 -0700

    updating home page

--- jishof/lib/main.dart
+++ jishof/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/SearchPage.dart';
+
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
@@ -21,16 +22,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
@@ -46,36 +37,20 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return new Scaffold(
       body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          child: new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+            new Image(
+              image: new AssetImage('assets/drawable/download.png'),
+              width: 264.0,
+              height: 128.0,
+            ),
             new Padding(
                 padding:
-                    new EdgeInsets.symmetric(vertical: 0.0, horizontal: 40.0),
-                    child: new TextField(
+                    new EdgeInsets.symmetric(vertical: 20.0, horizontal: 40.0),
+                child: new TextField(
                   decoration: const InputDecoration(
                       border: const OutlineInputBorder(
                           borderSide: BorderSide(color: Color(0x00000000))),
@@ -83,13 +58,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   textAlign: TextAlign.left,
                   controller: searchBarController,
                 )),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
+          ])),
       floatingActionButton: new FloatingActionButton(
         onPressed: _search,
         tooltip: 'Search',
