af836e6c863b4a025045ae0f1e3510d457268981
Timothy-Toombs <timothytoombs98@gmail.com>
2020-04-18 05:13:01 -0700

    rebuilding RomanKanaConverter so it works and adding test file

---
+++ versions 3.0+/jisho/lib/KanaConverters/RomanKanaConverterTest.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_app/KanaConverters/RomanKanaConverter.dart' as convert;
+void main() {
+  print("transliterating 'romaji' as :" + convert.transliterate("romaji"));
+  print("transliterating 'goemon' as :" + convert.transliterate("goemon"));
+  print("transliterating 'konna' as :" + convert.transliterate("konna"));
+  print("transliterating 'chikatetsu' as :" + convert.transliterate("chikatetsu"));
+  print("transliterating 'seppuku' as :" + convert.transliterate("seppuku"));
+  print("transliterating 'aoi' as :" + convert.transliterate("aoi"));
+  print("transliterating 'eiga' as :" + convert.transliterate("eiga"));
+  print("transliterating 'densha' as :" + convert.transliterate("densha"));
+  print("transliterating 'jinzya' as :" + convert.transliterate("jinzya"));
+  print("transliterating 'ginza' as :" + convert.transliterate("ginza"));
+  print("transliterating 'fukuoka' as :" + convert.transliterate("fukuoka"));
+  print("transliterating 'humei' as :" + convert.transliterate("humei"));
+  print("transliterating 'kakugou' as :" + convert.transliterate("kakugou"));
+
+}
